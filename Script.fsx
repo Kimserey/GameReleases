049714a4e7304b5c1ff35442764330f0af4cacf9
Kimserey Lam <Kimserey@users.noreply.github.com>
2016-07-20 23:03:51 +0100

    Update Script.fsx

--- Script.fsx
+++ Script.fsx
@@ -1,13 +1,38 @@
 #r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 
-let getGameInfo gameId =
-    let json =
-        Http.RequestString (
-            url     = "http://www.giantbomb.com/api/game/" + gameId, 
-            query   = [ "api_key", config.GiantBomb.ApiKey
-                        "format", "json"
-                        "field_list", "name,original_release_date" ])
-    printfn "%s" json
-    try Some <| GiantBomb.GameResponse.Parse json
-    with _ -> None
+open System
+open System.Globalization
+open System.IO
+open FSharp.Data
+open Newtonsoft.Json
+
+type Publisher = {
+    Name: string
+    Games: Game []
+} and Game = {
+    Id: int
+    Name: string
+    Release: DateTime
+}
+
+type HTMLGamePage = HtmlProvider<"http://www.giantbomb.com/mirror-s-edge-catalyst/3030-27028/">
+type BlizzardResponse = JsonProvider<"./json/atvi.json">
+
+let blizzard = BlizzardResponse.GetSample().Results
+
+let publisher = {
+    Name = blizzard.Name
+    Games =
+        blizzard.PublishedGames
+        |> Array.Parallel.map (fun game -> game, HTMLGamePage.Load(game.SiteDetailUrl))
+        |> Array.choose (fun (game, page) -> 
+            page.Tables.``Game details``.Rows
+            |> Seq.filter (fun row -> row.Column1 = "First release date" && (String.IsNullOrWhiteSpace row.Column2 |> not))
+            |> Seq.map (fun row -> Convert.ToDateTime(row.Column2))
+            |> Seq.tryHead
+            |> Option.map (fun date -> 
+                { Id = game.Id
+                  Name = game.Name
+                  Release = date }))
+}
